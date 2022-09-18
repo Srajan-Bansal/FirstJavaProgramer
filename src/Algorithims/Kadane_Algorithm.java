dbf60922d7c136caf322a76354d541d88d4101a4
Srajan Bansal <srajanbansal999@gmail.com>
2022-09-18 12:14:38 +0530

    Kadane_Algorithim has been added in src/Algorithims and Ignore all other changes

---
+++ src/Algorithims/Kadane_Algorithm.java
@@ -0,0 +1,20 @@
+package Algorithims;
+
+public class Kadane_Algorithm {
+    public static void main(String[] args) {
+        int[] arr = {1, -2,-3, 4, -1, 2, 1};
+
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+            maxSum = Math.max(maxSum, sum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        System.out.println(maxSum);
+    }
+}
