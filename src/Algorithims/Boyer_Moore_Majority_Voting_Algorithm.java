ad676edaf771ee8aabed8c795abbe2164cf59c5f
Srajan Bansal <srajanbansal999@gmail.com>
2022-09-25 10:04:17 +0530

    code changed - Value of intial count has been changed to 0 (int count = 0)

--- src/Algorithims/Boyer_Moore_Majority_Voting_Algorithm.java
+++ src/Algorithims/Boyer_Moore_Majority_Voting_Algorithm.java
@@ -4,7 +4,7 @@ public class Boyer_Moore_Majority_Voting_Algorithm {
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 1, 2, 3, 1, 5};
 
-        int count = 1;
+        int count = 0;
         int canditate = arr[0];
 
         for (int i = 0; i < arr.length; i++) {
