6a2932ab93febc7927b8a789eda5085f219a9a98
Srajan Bansal <srajanbansal999@gmail.com>
2022-10-14 08:12:59 +0530

    Reverse a Stack using Recursion

---
+++ src/ElementsOfProgramming/Stack/StackReversal.java
@@ -0,0 +1,25 @@
+package ElementsOfProgramming.Stack;
+
+import java.util.Stack;
+
+public class StackReversal {
+    static void reverse(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+        int item = stack.pop();
+        reverse(stack);
+        insert(stack, item);
+    }
+
+    static void insert(Stack<Integer> stack, int data) {
+        if (stack.isEmpty()) {
+            stack.push(data);
+            return;
+        }
+        int item = stack.pop();
+        insert(stack, data);
+        stack.push(item);
+    }
+
+}
